60de63b344f7c3d723d372a5bd0afb29386a1ee4
Diego Martinez <diegomrey1@gmail.com>
2025-05-28 20:10:21 +0200

    create navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 // import Image from "next/image";
 
+import NavBar from "@/components/layout/NavBar"
+
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <h1>Hello world!</h1>
     </div>
   );
